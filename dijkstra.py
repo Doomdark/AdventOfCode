28ca490906db7983a5e7b4b430d8be8f4d86b1aa
Doomdark <54108031+Doomdark@users.noreply.github.com>
2023-12-21 12:29:52 +0000

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -13,25 +13,25 @@ import heapq as heap
 ##                 G[(x,y)][(_x,_y)] = 1
 
 def dijkstra(G, startingNode):
-        visited = set()
-        parentsMap = {}
-        pq = []
-        nodeCosts = defaultdict(lambda: float('inf'))
-        nodeCosts[startingNode] = 0
-        heap.heappush(pq, (0, startingNode))
+    visited = set()
+    parentsMap = {}
+    pq = []
+    nodeCosts = defaultdict(lambda: float('inf'))
+    nodeCosts[startingNode] = 0
+    heap.heappush(pq, (0, startingNode))
 
-        while pq:
-                # go greedily by always extending the shorter cost nodes first
-                _, node = heap.heappop(pq)
-                visited.add(node)
+    while pq:
+        # go greedily by always extending the shorter cost nodes first
+        _, node = heap.heappop(pq)
+        visited.add(node)
 
-                for adjNode, weight in G[node].items():
-                        if adjNode in visited:  continue
+        for adjNode, weight in G[node].items():
+            if adjNode in visited:  continue
 
-                        newCost = nodeCosts[node] + weight
-                        if nodeCosts[adjNode] > newCost:
-                                parentsMap[adjNode] = node
-                                nodeCosts[adjNode] = newCost
-                                heap.heappush(pq, (newCost, adjNode))
+            newCost = nodeCosts[node] + weight
+            if nodeCosts[adjNode] > newCost:
+                parentsMap[adjNode] = node
+                nodeCosts[adjNode] = newCost
+                heap.heappush(pq, (newCost, adjNode))
 
-        return parentsMap, nodeCosts
+    return parentsMap, nodeCosts
