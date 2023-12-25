7e9667c548399992227170d5ecd75f61ae445af5
Doomdark <54108031+Doomdark@users.noreply.github.com>
2023-12-25 10:38:00 +0000

    Day 25

---
+++ 2023/25.py
@@ -0,0 +1,26 @@
+import random
+import networkx as nx
+
+lines = open('25.in').read().splitlines()
+
+wires = set()
+nodes = {}
+
+G = nx.Graph()
+
+# Make nodes
+for line in lines:
+    src, r = line.split(': ')
+    dests = r.split(' ')
+    for dest in dests:
+        G.add_edge(src, dest, capacity=1)
+
+# Use minimum cut to chop up the graph into pieces
+while True:
+    nodes = list(G.nodes())
+    a,b = random.choices(nodes, k=2)
+    if a == b: continue
+    cut, partition = nx.minimum_cut(G, a, b)
+    if cut == 3:
+        print('Part 1:', len(partition[0]) * len(partition[1]))
+        break
