e531e6d983995c17001c5bcaf8a6e36eac68e589
Doomdark <54108031+Doomdark@users.noreply.github.com>
2024-12-08 09:30:38 +0000

    Day 8 add visualisation

--- 2024/08.py
+++ 2024/08.py
@@ -23,15 +23,45 @@ def on_grid(loc):
     if c<0 or c>max_c: return False
     return True
 
-def print_grid(antinodes):
-    global allnodes
+antinodes_prev = set()
+CLEAR   = '\033[2J'
+DEFAULT = '\033[0m'
+BOLD    = '\033[1m'
+RED     = '\033[91m'
+GREEN   = '\033[32m'
+BOLD_GREEN   = '\033[32;1m'
+YELLOW  = '\033[93m'
+BLUE    = '\033[94m'
+PURPLE  = '\033[95m'
+CYAN    = '\033[96m'
+WHITE   = '\033[97m'
+GREY    = '\033[38;5;8m'
+
+import copy, time
+
+visualise = True
+
+def print_grid(a,b,antinodes):
+    global allnodes, antinodes_prev
+    print(CLEAR,'-'*25)
     for r in range(max_r+1):
         l = ''
         for c in range(max_c+1):
-            if (r,c) in allnodes.keys(): l += (allnodes[(r,c)])
-            elif (r,c) in antinodes: l += '#'
-            else: l+= '.'
+            if (r,c) in allnodes.keys():
+                # Print generators in green
+                green = GREEN
+                # And the active ones in bold
+                if (r,c) in [a,b]:
+                    green = BOLD
+                l += '{}{}{}'.format(green,allnodes[(r,c)],DEFAULT)
+            elif (r,c) in antinodes:
+                if (r,c) not in antinodes_prev:
+                    l += '{}#{}'.format(BOLD, DEFAULT)
+                else:
+                    l += '#'
+            else: l+= '{}.{}'.format(GREY,DEFAULT)
         print(l)
+    antinodes_prev = copy.copy(antinodes)
 
 # Find antinodes first by looking at pairs of nodes
 def solve(nodes, part2=False):
@@ -84,8 +114,12 @@ def solve(nodes, part2=False):
                     na = (nar,nac)
                     nb = (nbr,nbc)
                     # Add antinodes if they're on the grid
-                    if on_grid(na): antinodes.add(na)
-                    if on_grid(nb): antinodes.add(nb)
+                    if on_grid(na):
+                        antinodes.add(na)
+                        if part2 and visualise: print_grid(a,b,antinodes)
+                    if on_grid(nb):
+                        antinodes.add(nb)
+                        if part2 and visualise: print_grid(a,b,antinodes)
                     # Exit here if we're off the grid, or it's part 1
                     if part2:
                         # Stop making antinodes if both directions are off the grid
@@ -97,8 +131,9 @@ def solve(nodes, part2=False):
                     # Next locations for part 2
                     ar,ac = na
                     br,bc = nb
+
+                    if visualise: time.sleep(0.1)
     return antinodes
 
-#print_grid(solve(nodes, True))
 print('Part 1:', len(solve(nodes)))
 print('Part 2:', len(solve(nodes, True)))
