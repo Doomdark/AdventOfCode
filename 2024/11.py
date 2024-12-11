b8269db2c6d754d2f082fc1a338d73df7340e064
Doomdark <54108031+Doomdark@users.noreply.github.com>
2024-12-11 08:06:26 +0000

    Update 11.py

--- 2024/11.py
+++ 2024/11.py
@@ -1,34 +1,30 @@
-STONES = '773 79858 0 71 213357 2937 1 3998391'
+stones = '773 79858 0 71 213357 2937 1 3998391'.split()
 
-stones = tuple(STONES.split())
+from collections import defaultdict
 
-def apply(stones):
+def apply(stone):
     _stones = []
-    for stone in stones:
-        istone = int(stone)
-        if istone == 0:
-            _stones += ['1']
-        elif len(stone)%2 == 0:
-            half = len(stone)>>1
-            _stones += [ str(int(stone[:half])), str(int(stone[half:])) ]
-        else:
-            _stones += [ str(istone*2024) ]
-    return _stones
+    istone = int(stone)
+    if istone == 0:
+        _stones += ['1']
+    elif len(stone)%2 == 0:
+        half = len(stone)>>1
+        _stones += [ str(int(stone[:half])), str(int(stone[half:])) ]
+    else:
+        _stones += [ str(istone*2024) ]
+    return tuple(_stones)
 
-for i in range(25):
-    stones = apply(stones)
+def solve(loops=25):
+    global stones
+    S = {stone:1 for stone in stones}
+    for i in range(loops):
+        _S = defaultdict(int)
+        for stone, count in S.items():
+            _stones = apply(stone)
+            for s in _stones:
+                _S[s] += count
+        S = _S
+    return sum(S.values())
 
-print('Part 1:', len(stones))
-
-stones = STONES.split()
-
-nstones = []
-# Do each stone individually
-for j,stone in enumerate(stones):
-    _stones = [stone]
-    for i in range(75):
-        _stones = apply(_stones)
-        print(j,i, len(_stones))
-    nstones.append(len(_stones))
-
-print('Part 2:', sum(nstones))
+print('Part 1:', solve())
+print('Part 2:', solve(75))
