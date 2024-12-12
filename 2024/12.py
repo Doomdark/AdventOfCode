a1a223f6adecd3b4b05ff8185807edea6e68dca4
Doomdark <54108031+Doomdark@users.noreply.github.com>
2024-12-12 15:12:09 +0000

    Update 12.py

--- 2024/12.py
+++ 2024/12.py
@@ -1,8 +1,6 @@
-lines = open('12test3.in').read().splitlines()
+lines = open('12test4.in').read().splitlines()
 
 from collections import defaultdict
-import sys
-sys.setrecursionlimit(10*6)
 
 regions = defaultdict(list)
 
@@ -17,35 +15,33 @@ def adjacent(area, r, c):
             count += 1
     return count
 
-def flood(AREA, name, r, c):
+def flood(name, r, c):
     global lines
-    AREA.add((r,c))
-    for dr,dc in [(0,1), (0,-1), (1,0), (-1,0)]:
-        nr,nc = r+dr,c+dc
-        if 0<nr<max_r and 0<nc<max_c:
-            if lines[nr][nc] == name and (nr,nc) not in AREA:
-                flood(AREA, name, nr, nc)
+    Q = [(r,c)]
+    AREA = set()
+    while Q:
+        r,c = Q.pop()
+        AREA.add((r,c))
+        for dr,dc in [(0,1), (0,-1), (1,0), (-1,0)]:
+            nr,nc = r+dr,c+dc
+            if nr>=0 and nr<max_r and nc>=0 and nc<max_c:
+                if (lines[nr][nc] == name) and ((nr,nc) not in AREA):
+                    Q.append((nr,nc))
+    return AREA
 
 SEEN = set()
 for r,line in enumerate(lines):
     for c,char in enumerate(line):
         if (r,c) not in SEEN:
-            AREA = set()
-            flood(AREA, char, r, c)
+            AREA = flood(char, r, c)
             SEEN |= AREA
-            if char == 'R':
-                print(SEEN)
             regions[char].append(list(AREA))
-
-print(regions['R'])
 
 def get_perimeter(area):
     total = 0
-    #print(area)
     for r,c in area:
         open_sides = 4-adjacent(area, r, c)
         total += open_sides
-        #print('-',r, c, open_sides)
     return total
 
 def get_area(area):
@@ -54,10 +50,93 @@ def get_area(area):
 total = 0
 for name, area_list in regions.items():
     for area in area_list:
-        #print(name, area)
         _area = get_area(area)
         _perimeter = get_perimeter(area)
-        print(name, _area, _perimeter)
         total += _area * _perimeter
 
 print('Part 1:', total)
+
+# Direction if we were going straight on
+left     = {(0,1):(-1,0),(-1,0):(0,-1),(0,-1):(1,0),(1,0):(0,1)}
+straight = {(0,1):(0,1),(-1,0):(-1,0),(0,-1):(0,-1),(1,0):(1,0)}
+right    = {(0,1):(1,0),(1,0):(0,-1),(0,-1):(-1,0),(-1,0):(0,1)}
+D        = {(0,1):'>',(1,0):'v',(-1,0):'^',(0,-1):'<'}
+
+def check(area, direction, loc, lloc):
+    r,c = loc
+    dr,dc = direction
+    nr,nc = r+dr,c+dc
+    if (nr,nc) in area and (nr,nc) != lloc:
+        return True
+    return False
+
+def move(loc, direction):
+    r,c = loc
+    dr,dc = direction
+    return (r+dr,c+dc)
+
+import time
+
+def get_sides(area):
+    # A single point has 4 sides
+    #if len(area) == 1:
+    #    return 4
+    # Start at a corner as edges start there. We're counting the corners really.
+    min_c = min([c for r,c in area])
+    min_r = min([r for r,c in area if c == min_c])
+    loc = (min_r, min_c)
+    start = (min_r, min_c)
+    # Start bt going along the row from left to right
+    direction = (0,1)
+    print('start:', start)
+    corners = 0
+    VISITED = set(start)
+    lloc = start
+    # Now we need to trace the edge
+    # We can only turn 90 degrees so tracing needs to turn left first, then straight, then right.
+    while True:
+        turn = False
+        nloc = loc
+        ndirection = direction
+        VISITED.add(loc)
+        #time.sleep(0.2)
+        # Try to turn first before we move
+        # Does the left-hand square exist in the area?
+        if check(area, left[direction], loc, lloc):
+            turn = True
+            ndirection = left[direction]
+        # Straight on
+        elif check(area, direction, loc, loc):
+            pass
+        # Always turn right if we can't turn left or move forward
+        else:
+            turn = True
+            ndirection = right[direction]
+        if not turn:
+            lloc = loc
+            nloc = move(loc, ndirection)
+        if turn:
+            corners += 1
+        print(loc, nloc, D[direction], turn, D[ndirection], corners)
+        if nloc == start and ndirection == (0,1):
+            return corners, VISITED
+        loc = nloc
+        direction = ndirection
+
+for line in lines:
+    print(line)
+    
+total = 0
+
+for name, area_list in regions.items():
+    for area in area_list:
+        # Get the sides counts
+        print(name, area)
+        _area = get_area(area)
+        _sides, visited = get_sides(area)
+        print(_area, _sides)
+        total += _area * _sides
+
+# At this point we have all the outer side counts.
+
+print('Part 2:', total)
