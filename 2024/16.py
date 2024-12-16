b8a00d0dac62a2fed24b921e4ee4cdd67d9c4d10
Doomdark <54108031+Doomdark@users.noreply.github.com>
2024-12-16 19:58:43 +0000

    Update 16.py

--- 2024/16.py
+++ 2024/16.py
@@ -13,85 +13,66 @@ walls = set()
 
 for r,line in enumerate(lines):
     for c,char in enumerate(line):
-        if char == '#': walls.add((r,c))
+        if   char == '#': walls.add((r,c))
         elif char == 'S': S = (r,c)
         elif char == 'E': E = (r,c)
 
-def print_grid(path):
-    for r in range(R):
-        l = ''
-        for c in range(C):
-            if   (r,c) in walls: l += '#'
-            elif (r,c) in path:  l += 'O'
-            else: l += '.'
-        print(l)
+LEFT  = {(-1,0):(0,-1), (0,-1):(1,0), (1,0):(0,1), (0,1):(-1,0)}
+RIGHT = {(-1,0):(0,1), (0,1):(1,0), (1,0):(0,-1), (0,-1):(-1,0)}
 
-def solve(start, end, start_dir=(1,0), reverse=False):
+def solve(start, end):
     Q = []
-    if reverse:
-        for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)]:
-            heapq.heappush(Q, (start,0,set(),(dr,dc),[]) )
-    else:
-        heapq.heappush(Q, (start,0,set(),start_dir,[]) )
+    heapq.heappush(Q, (start,0,set(),(0,1),[]) )
 
     min_cost = None
-    nodes = defaultdict()
     best_paths = defaultdict()
-    
+    nodes = defaultdict()
+
     while Q:
-        loc,cost,SEEN,ldir,path = heapq.heappop(Q)
+        loc, cost, SEEN, dir, path = heapq.heappop(Q)
         # Accumulate the path
-        npath = path + [loc]
+        newpath = path + [loc]
         # Reached the end of the line. Well it's allllllllright... ridin' around in the breeze...
         if loc == end:
             # If the cost is lower than the current min cost then that's the best path
             if min_cost is None or cost <= min_cost:
-                if cost not in best_paths.keys():
-                    best_paths[cost] = set()
-                best_paths[cost] |= set(npath)
                 min_cost = cost
-                print(min_cost)
+                # Store the best paths for part 2
+                if cost not in best_paths:
+                    best_paths[cost] = set()
+                best_paths[cost].update(set(newpath))
+                #print(min_cost)
             continue
         # Been here before?
-        if loc in SEEN: continue
-        # Nope
+        if (loc,dir) in SEEN: continue
+        # Nope, new location
         _SEEN = copy.copy(SEEN)
-        _SEEN.add(loc)
+        _SEEN.add((loc,dir))
         # If the path cost is already higher than the min cost then this path is bogus
         if min_cost is not None and cost >= min_cost:
             continue
-        # Add a cost to the current node including direction
-        if (loc,ldir) in nodes.keys():
+        # Have we reached here before?
+        if (loc,dir) in nodes.keys():
             # If the current cost on entering from the same direction is higher than however we got here before then ignore this path
-            if cost > nodes[(loc,ldir)]: continue
-        nodes[(loc,ldir)] = cost
+            if cost > nodes[(loc,dir)]: continue
+        # Add the cost for this node
+        nodes[(loc,dir)] = cost
+        # Current r,c
         r,c = loc
-        # Try to go in every direction
-        for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+        # Try to go forward, left and right
+        for dr,dc in [dir, LEFT[dir], RIGHT[dir]]:
             cost_add = 1
-            nr,nc = r+dr,c+dc
-            if (nr,nc) not in walls:
-                # Cost is +1000 if we didn't go in the same direction as last time
-                if ldir != (dr,dc): cost_add += 1000
-                ncost = cost_add+cost
-                #Q.append( ((nr,nc),ncost,_SEEN,(dr,dc),npath) )
-                heapq.heappush(Q, ((nr,nc),ncost,_SEEN,(dr,dc),npath) )
+            newloc = (r+dr,c+dc)
+            newdir = (dr,dc)
+            if newloc not in walls:
+                # Cost is an extra +1000 if we didn't go in the same direction as last time
+                if dir != newdir:
+                    cost_add += 1000
+                heapq.heappush(Q, (newloc, cost+cost_add, _SEEN, newdir, newpath) )
 
-    return min_cost, best_paths, nodes
+    return min_cost, best_paths
 
-min_cost, p1_paths, p1_nodes = solve(S,E)
+min_cost, paths = solve(S,E)
+
 print('Part 1:', min_cost)
-
-#print_grid(best_paths[min_cost])
-
-#_, p2a_paths = solve(E,S,(-1,0))
-_, p2b_paths, p2_nodes = solve(E,S,(0,-1), True)
-
-OK = set()
-for r in range(R):
-    for c in range(C):
-        for dir in [(1,0),(-1,0),(0,1),(0,-1)]:
-            # (r,c,dir) is on an optimal path if the distance from start to end equals the distance from start to (r,c,dir) plus the distance from (r,c,dir) to end.
-            if ((r,c),dir) in p1_nodes and ((r,c),dir) in p2_nodes and p1_nodes[((r,c),dir)] + p2_nodes[((r,c),dir)] == min_cost:
-                OK.add((r,c))
-print('Part 2:', len(OK))
+print('Part 2:', len(paths[min_cost]))
