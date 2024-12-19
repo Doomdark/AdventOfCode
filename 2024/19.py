a78c90912325be1427328f787314a4aee39390c3
Doomdark <54108031+Doomdark@users.noreply.github.com>
2024-12-19 09:01:23 +0000

    Update 19.py

--- 2024/19.py
+++ 2024/19.py
@@ -12,13 +12,14 @@ def dfs(towel, O=0):
     if (towel,O) in DP:
         return DP[(towel,O)]
     ways = 0
+    # Test against patterns which fit entirely into the remaining towel length
     PATTERNS = [x for x in patterns if len(x) <= len(towel[O:]) and x]
-    for P in sorted(PATTERNS, key=lambda s: len(s)):
+    for P in PATTERNS:
         if towel[O:].startswith(P):
             # If the pattern length matches the remaining towel then it's a complete match
             if len(P) == len(towel[O:]):
                 ways += 1
-            elif O+len(P) <= len(towel):
+            else:
                 ways += dfs(towel, O+len(P))
     # Cache the result for later
     DP[(towel,O)] = ways
