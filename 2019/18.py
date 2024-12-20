90863cc2fe7924c84262c1c7cc81026d97ddff08
Doomdark <54108031+Doomdark@users.noreply.github.com>
2024-12-20 16:15:58 +0000

    Update 18.py

--- 2019/18.py
+++ 2019/18.py
@@ -1,195 +1,118 @@
 from collections import defaultdict, deque
 import heapq as heap
-import copy
 
-def dijkstra(G, startingNode, avoid=set()):
-    visited = set()
-    parentsMap = {}
-    pq = []
-    nodeCosts = defaultdict(lambda: float('inf'))
-    nodeCosts[startingNode] = 0
-    paths = {}
-    heap.heappush(pq, (0, startingNode, []))
-
-    while pq:
-        # go greedily by always extending the shorter cost nodes first
-        _, node, path = heap.heappop(pq)
-        visited.add(node)
-
-        for adjNode, weight in G[node].items():
-            if adjNode in visited: continue
-            if adjNode in avoid: continue
-
-            newCost = nodeCosts[node] + weight
-            if nodeCosts[adjNode] > newCost:
-                parentsMap[adjNode] = node
-                nodeCosts[adjNode] = newCost
-                adjPath = path + [adjNode]
-                paths[adjNode] = path
-                heap.heappush(pq, (newCost, adjNode, adjPath))
-
-    return parentsMap, nodeCosts, paths
-
-walls = set()
-doors = defaultdict(int)
-keys  = defaultdict(int)
+WALLS = set()
+DOORS  = defaultdict(int)
+DDOORS = defaultdict(int)
+KEYS   = defaultdict(int)
+KKEYS  = defaultdict(int)
 
 start = (0,0)
 
 lines = open('18.in').read().splitlines()
 
-R = 0
-C = 0
+R = len(lines)
+C = len(lines[0])
+locs = []
+grid = []
 
 for r,row in enumerate(lines):
+    line = []
     for c,col in enumerate(row):
+        line.append(col)
         loc = (r,c)
-        if   col == '#' : walls.add(loc)
-        elif col == '@' : start = loc
-        elif ord(col) in range(ord('a'),ord('z')+1): keys[loc] = col
-        elif ord(col) in range(ord('A'),ord('Z')+1): doors[loc] = col
-
-R = r+1
-C = c+1
+        if col == '@' : start = loc
+        elif ord(col) in range(ord('a'),ord('z')+1):
+            KEYS[loc] = col
+            KKEYS[col] = loc
+        elif ord(col) in range(ord('A'),ord('Z')+1):
+            DOORS[loc] = col
+            DDOORS[col] = loc
+    grid.append(line)
 
 # Draw the current state
 def draw_grid():
-    global walls,doors,keys
+    global WALLS,DOORS,KEYS,locs
     for r in range(R):
         row = ''
         for c in range(C):
             loc = (r,c)
-            if   loc in walls: row += '#'
+            if   grid[r][c] == '#': row += '#'
             elif loc == start: row += '@'
-            elif loc in keys:  row += keys[loc]
-            elif loc in doors: row += doors[loc]
+            elif loc in locs:  row += '@'
+            #elif loc in locs:  row += '@'
+            elif loc in KEYS:  row += KEYS[loc]
+            elif loc in DOORS: row += DOORS[loc]
             else:              row += '.'
         print(row)
 
-draw_grid()
+def reachable_keys(loc, keys):
+    "Which keys can we get to from here that aren't in the provided keys list?"
+    Q = deque()
+    Q.append((0, loc))
+    SEEN = set()
 
-# Get the adjacent locations which aren't walls
-def get_adjacents(loc):
-    adjacents = []
-    r,c = loc
-    for dr,dc in [(-1,0),(1,0),(0,1),(0,-1)]:
-        nr,nc = r+dr,c+dc
-        if (nr,nc) not in walls:
-            adjacents.append((nr,nc))
-    return adjacents
-
-# Make a connectivity graph
-G = defaultdict(dict)
-for r in range(R):
-    for c in range(C):
-        if (r,c) in walls: continue
-        adjacents = get_adjacents((r,c))
-        for adj in adjacents:
-            G[(r,c)][adj] = 1
-
-# For all keys make a list of how far it is to all other keys, ignoring blockages
-Mins = defaultdict(lambda:100000000)
-min_steps_between_keys = 1000000
-for k in keys:
-    parentMap, nodeCosts, paths = dijkstra(G, k)
-    for kk in keys:
-        if k == kk:
+    while Q:
+        steps, loc = Q.popleft()
+        if loc in SEEN: continue
+        SEEN.add(loc)
+        # It's a key but we don't have it yet
+        if loc in KEYS and loc not in keys:
+            yield steps, loc
             continue
-        min_steps_between_keys = min(min_steps_between_keys, nodeCosts[kk])
-        Mins[k] = min(Mins[k],nodeCosts[kk])
+        # Move to the adjacent squares
+        r,c = loc
+        for dr,dc in [(-1,0),(1,0),(0,1),(0,-1)]:
+            nr,nc = r+dr,c+dc
+            nloc = (nr,nc)
+            if grid[nr][nc] == '#': continue
+            # Can't go through a locked door
+            if nloc in DOORS and KKEYS[DOORS[nloc].lower()] not in keys: continue
+            # Otherwise we can move
+            Q.append((steps+1, nloc))
 
-def solve():
-    global R,C,G
+def solve(locs):
+    Q = []
+    heap.heappush(Q, (0, locs, frozenset()))
+    SEEN = set()
 
-    q = deque()
-    # Current loc, steps, owned keys, open doors
-    q.append( ( start, 0, frozenset(), frozenset() ) )
-    visited = defaultdict(lambda:10000000)
+    while Q:
+        steps, clocs, keys = heap.heappop(Q)
+        # If we've got all the keys then that's the minimum route
+        if len(KEYS) == len(keys):
+            return steps
+        # Been here before?
+        if (clocs, keys) in SEEN: continue
+        SEEN.add((clocs,keys))
+        # Try to get to all the other keys
+        for i in range(len(clocs)):
+            for dsteps, dloc in reachable_keys(clocs[i], keys):
+                # Update locs
+                dlocs = clocs[:i] + (dloc,) + clocs[i+1:]
+                heap.heappush(Q, (steps+dsteps, dlocs, keys | frozenset([dloc]) ) )
 
-    min_steps = 10000000
+locs = []
+locs.append(start)
+locs = tuple(locs)
+min_steps = solve(locs)
+print('Part 1:', solve(locs))
 
-    while q:
-        cur_loc, cur_steps, cur_keys, open_doors = q.pop()
-        print(len(q), min_steps, cur_steps, len(cur_keys), len(open_doors))
+# -- Part 2 --
 
-        # Any more keys to collect?
-        if len(cur_keys) == len(keys):
-            if cur_steps < min_steps:
-                print(cur_steps),
-            min_steps = min(min_steps, cur_steps)
+# Update the walls to chop the map into into 4 quadrants
 
-        else: # More keys to collect
-            # How many keys left?
-            keys_left = [kloc for kloc,v in keys.items() if v not in cur_keys]
-            min_steps_remaining = sum([Mins[k] for k in keys_left])
-            # If the number of keys left * min distance between keys + cur_steps > min steps then stop here
-            if (cur_steps + min_steps_remaining) >= min_steps:
-                continue
-            # Get paths from here to everywhere avoiding the closed doors
-            parentsMap, nodeCosts, paths = dijkstra(G,cur_loc,set(doors.keys())-open_doors)
-            # Iterate through the keys
-            for key_loc,key in keys.items():
-                # If we've got this key then skip it
-                if key in cur_keys:
-                    continue
-                # Can get to this key?
-                if key_loc in nodeCosts:
-                    #print('Cost to', key, nodeCosts[key_loc])
-                    new_steps  = cur_steps + nodeCosts[key_loc]
-                    # Already gone further than the current min_steps
-                    if new_steps >= min_steps:
-                        continue
-                    new_loc = key_loc
-                    new_open_doors = copy.copy(open_doors)
-                    # Add the corresponding door to the open doors list
-                    for door_loc,door in doors.items():
-                        if ord(key)-32 == ord(door):
-                            new_open_doors = open_doors | {door_loc}
-                            break
-                    new_keys = cur_keys | {key}
-                    new_state = (new_loc, new_keys)
-                    if visited[new_state] > new_steps:
-                        q.append((new_loc, new_steps, new_keys, new_open_doors))
-                        visited[new_state] = new_steps
+# Original start position becomes a wall
+grid[start[0]][start[1]] = '#'
 
-    return min_steps
+# N, E, S, W of the start position also become walls
+for dr,dc in [(-1,0),(1,0),(0,1),(0,-1)]:
+    nr,nc = dr+start[0], dc+start[1]
+    grid[nr][nc] = '#'
 
-def got_key(keys, door):
-    for key in keys:
-        if ord(key)-32 == ord(door):
-            return True
-    return False
-
-def solve2():
-    q = deque()
-    visited = defaultdict(lambda:10000000)
-    q.append((start, 0, frozenset()))
-    min_steps = 100000000
-
-    while q:
-        cur_loc, cur_steps, cur_keys = q.popleft()
-        #print(len(q), cur_loc, cur_steps, cur_keys, len(cur_keys))
-
-        # Found a key!
-        if cur_loc in keys:
-            cur_keys = cur_keys | {keys[cur_loc]}
-            # Any more keys to collect?
-            if len(cur_keys) == len(keys):
-               return cur_steps
-        # Got to a door but can't open it
-        elif cur_loc in doors and not got_key(cur_keys, doors[cur_loc]):
-            continue
-
-        # More keys needed
-        state = (cur_keys, cur_loc)
-        # Got to this state before in fewer steps
-        if visited[state] < cur_steps:
-            continue
-        # Update the step count
-        visited[state] = cur_steps
-
-        for loc in get_adjacents(cur_loc):
-            q.append((loc, cur_steps+1, cur_keys))
-
-min_steps = solve()
-print('Part 1:', min_steps)
+# Start locations for the 4 robots are diagonals one step from the original start
+locs = []
+for i,(dr,dc) in enumerate([(-1,1),(1,-1),(1,1),(-1,-1)]):
+    nr,nc = dr+start[0], dc+start[1]
+    locs.append((nr,nc))
+locs = tuple(locs)
+print('Part 2:', solve(locs))
