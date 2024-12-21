948c5522d659b14ee302bada9e8b122c2379bff9
Doomdark <54108031+Doomdark@users.noreply.github.com>
2024-12-21 12:38:09 +0000

    Update 21.py

--- 2024/21.py
+++ 2024/21.py
@@ -2,18 +2,22 @@ from collections import defaultdict, deque
 
 codes = open('21.in').read().splitlines()
 
-# Make the keypads
+# Make the numeric keypad
 n = ['789','456','123','.0A']
 nR,nC = 4,3
+N = {}
+
+# Make the direction keypad
 d = ['.^A', '<v>']
 dR,dC = 2,3
-N = {}
 D = {}
 
 def md(a,b):
+    'Manhattan distance'
     return abs(b[0]-a[0]) + abs(b[1]-a[1])
 
 def paths(grid,r,c,R,C):
+    'Find all the shortest paths to the other squares on the provided grid starting from (r,c).'
     Q = deque()
     Q.append((0,r,c,'',0))
     paths = {}
@@ -31,6 +35,7 @@ def paths(grid,r,c,R,C):
         # Go to each other point
         for dr,dc,d in [(0,1,'>'),(0,-1,'<'),(1,0,'v'),(-1,0,'^')]:
             nr,nc = cr+dr,cc+dc
+            # Get the Manhattan distance between the two points
             MD = md((r,c),(nr,nc))
             # Use Manhattan distance to ensure we always move away from the source button
             if 0<=nr<R and 0<=nc<C and grid[nr][nc] != '.' and MD == lmd+1:
@@ -42,13 +47,13 @@ def paths(grid,r,c,R,C):
         ans[char] = lens[shortest]
     return ans
 
-# Get all possible routes from each point to each other point
+# Get all shortest distances from each key to each other key
 for r,line in enumerate(n):
     for c,char in enumerate(line):
         if n[r][c] != '.':
             N[char] = paths(n,r,c,nR,nC)
 
-# Get all possible routes from each point to each other point
+# Get all shortest distances from each key to each other key
 for r,line in enumerate(d):
     for c,char in enumerate(line):
         if d[r][c] != '.':
@@ -58,37 +63,40 @@ for r,line in enumerate(d):
 
 # Do a DFS function which recursively determines the shortest set of button presses starting from keypad N and digit M (posn)
 
+# Cache the results
 DP = {}
-def dfs(depth, digit, posn='A'):
+
+def dfs(DEPTH, CHAR, POSN='A'):
     # Return a value from the cache if there's an appropriate entry
-    if (depth,digit,posn) in DP:
-        return DP[(depth,digit,posn)]
+    if (DEPTH,CHAR,POSN) in DP:
+        return DP[(DEPTH,CHAR,POSN)]
     # Which keypad for this depth?
-    keypad = keypads[depth]
-    # Paths to get to that digit from A as we always start from A
-    paths = keypad[posn][digit]
-    # Is this the last keypad?
-    last = depth+1 == len(keypads)
-    # If we're last in the chain then choose path 0 from the list as it doesn't matter
-    if last:
-        DP[(depth,digit,posn)] = len(paths[0])
+    keypad = keypads[DEPTH]
+    # Paths to get to CHAR from POSN
+    paths = keypad[POSN][CHAR]
+    # If we're last in the keypad chain then choose path 0 from the list as they're all the same length
+    if DEPTH+1 == len(keypads):
+        # Store the length in the cache
+        DP[(DEPTH,CHAR,POSN)] = len(paths[0])
         return len(paths[0])
-    # OK, now try to figure out the shortest path from here to the end
-    PATH = 0
-    shortest = None
-    # Iterate over the shortest paths in the list
+    # OK, now try to figure out the shortest path from here to the end. Start with a chunky shortest path length.
+    shortest = 10**12
+    # Iterate over the shortest paths in the list for this keypad
     for path in paths:
-        # For this path we always start at A
-        _path = 0
-        POSN = 'A'
-        # A path looks like this ^v<A
-        for _digit in path:
-            _path += dfs(depth+1, _digit, POSN)
-            POSN =_digit
-        if shortest is None or _path < shortest:
-            shortest = _path
+        # Zero length to start with for each path
+        current = 0
+        # We always start at A
+        posn = 'A'
+        # A path looks something like this: ^v<A
+        for char in path:
+            # Recurse to the next keypad in the chain
+            current += dfs(DEPTH+1, char, posn)
+            # Continue from the previous char
+            posn = char
+        # Is the current path shorter than the current shortest?
+        shortest = min(shortest,current)
     # Store the shortest length in the cache
-    DP[(depth,digit,posn)] = shortest
+    DP[(DEPTH,CHAR,POSN)] = shortest
     # Return the shortest length
     return shortest
 
@@ -100,40 +108,25 @@ def get_complexity(paths):
         total += _c * p
     return total
 
-# -- Part1 --
+# -- Run --
 
-# Evaluate part 1 with a small number of keypads
-keypads = [N,D,D]
+# The last set of directions is in the last pad so we don't need it
+for part,pads in [(1,2),(2,25)]:
+    # Make a list of pads to traverse
+    keypads = [N] + [D] * pads
+    # Clear the cache
+    DP = {}
+    # List of shortest path lengths for each code
+    PATHS = []
+    # Test each code
+    for code in codes:
+        PATH = 0
+        # Start at A
+        posn = 'A'
+        # Find the shortest path length for each char. The next search starts from the previous one.
+        for char in code:
+            PATH += dfs(0, char, posn)
+            posn = char
+        PATHS.append(PATH)
 
-total = 0
-PATHS = []
-for code in codes:
-    PATH = 0
-    posn = 'A'
-    for digit in code:
-        PATH += dfs(0, digit, posn)
-        posn = digit
-    PATHS.append(PATH)
-
-print('Part 1:', get_complexity(PATHS))
-
-# -- Part 2 --
-
-# Evaluate part 1 with a chunky number of keypads
-keypads = [N,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D,D]
-
-# Reset the cache
-DP = {}
-
-# Evaluate
-total = 0
-PATHS = []
-for code in codes:
-    PATH = 0
-    posn = 'A'
-    for digit in code:
-        PATH += dfs(0, digit, posn)
-        posn = digit
-    PATHS.append(PATH)
-
-print('Part 2:', get_complexity(PATHS))
+    print(f'Part {part}:', get_complexity(PATHS))
