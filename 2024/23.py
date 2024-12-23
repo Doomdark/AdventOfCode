fd7a915a73c830a1863636f96c0338b322449011
Doomdark <54108031+Doomdark@users.noreply.github.com>
2024-12-23 11:29:09 +0000

    Update 23.py

--- 2024/23.py
+++ 2024/23.py
@@ -13,60 +13,57 @@ for line in lines:
     Adjacent[r].append(l)
     Adjacent[l].append(r)
 
-def get_connected(num=3):
-    groups = set()
-    for comb in combinations(Adjacent.keys(),num):
-        comb0 = list(comb)
-        connected = True
-        # Test all keys against all
-        for n in range(len(comb)):
-           a = comb0[0]
-           within = all([a in Adjacent[c] for c in comb0[1:]])
-           if not within:
-               connected = False
-               break
-           comb0 = comb0[1:] + [comb0[0]]
-        if connected:
-            groups.add(tuple(sorted(list(comb))))
-    return groups
+Paths = set()
 
-c = get_connected(3)
+def get_connected(nodes, depth, max_depth, path, part2=False):
+    # Ran out of connection depth so we've found a group of the required size.
+    if depth == max_depth:
+        if part2:
+            # Return an empty list to append the node tree to.
+            return []
+        else:
+            # Add the accumulated sorted path to the set of paths
+            Paths.add(tuple(sorted(list(path))))
+            return
+
+    # For each node in the provided list try to get the connections
+    for n in nodes:
+        # Get all the nodes adjacent to this node in the nodes list
+        adjacent_nodes = [x for x in nodes if x!=n and (x in Adjacent[n])]
+        # Use an iterator for part 2 as it only returns one item at a time so the
+        # end of the tree is reached really fast as we only care how deep it is.
+        if part2:
+            adjacent_nodes = iter(adjacent_nodes)
+        # Now find the connectivity on all the nodes in the adjacent list
+        ret = get_connected(adjacent_nodes, depth+1, max_depth, path + [n], part2)
+        # For part 2, if the function returned a list then add this node to the list and return it.
+        # Otherwise there were no connections.
+        if part2:
+            if ret is not None:
+                ret.append(n)
+                return ret
+
+    # If we got here then there are no nodes to connect to
+    return
+
+get_connected(list(Nodes), 0, 3, [])
+
 part1 = 0
-for cc in c:
-    if any([ccc.startswith('t') for ccc in cc]):
+for p in Paths:
+    # Check each computer in turn that it starts with a 't'
+    if any([pp.startswith('t') for pp in p]):
         part1 += 1
 print('Part 1:', part1)
 
 # -- Part 2 --
 
-# Part 1 takes ages. Try something else for part 2.
-
 # We only need the first group of any given size for part 2.
 
-def get_connected2(nodes, remaining):
-    # Ran out of connection depth so we've found a group of the required size.
-    # Return an empty list to append the node tree to.
-    if remaining == 0:
-        return []
-    # For each node in the provided list try to get the connections
-    for n in nodes:
-        # Get all the nodes adjacent to this node in the nodes list
-        adjacent_nodes = filter(lambda x: x!=n and (x in Adjacent[n]), nodes)
-        # Now do the same thing on all the adjacent nodes
-        ret = get_connected2(adjacent_nodes, remaining-1)
-        # If the function returned a list then add this node to the list and return it.
-        # Otherwise there were no connections.
-        if ret is not None:
-            ret.append(n)
-            return ret
-    # If we got here then there are no nodes to connect to
-    return None
-
-# Try group sizes upwards from 4 as that's where we stopped in part 1
+# Try group sizes upwards from 4 as that's +1 where we stopped in part 1
 part2 = None
 size = 4
 while True:
-    l = get_connected2(Nodes, size)
+    l = get_connected(list(Nodes), 0, size, [], True)
     # If we didn't find anything of that size then the previous value is the answer.
     if l is None:
         break
