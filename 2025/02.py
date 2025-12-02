d29561e5ef2241705ff87fd0fa5dde9fec5664a1
Doomdark <54108031+Doomdark@users.noreply.github.com>
2025-12-02 08:47:08 +0000

    Update 02.py

--- 2025/02.py
+++ 2025/02.py
@@ -5,15 +5,19 @@ ranges = line.split(',')
 
 invalid = 0
 
+# Iterate over the ranges
 for RANGE in ranges:
+    # Get the extents
     l,r = [int(x) for x in RANGE.split('-')]
+    # Iterate over this range
     for i in range(l,r+1):
         str_i = str(i)
-
+        # if the length isn't even then it's not a match
         if len(str_i) %2 == 0:
+            # Split the string in 2
             L = str_i[:len(str_i)//2]
             R = str_i[len(str_i)//2:]
-
+            # if the two match then that's a win
             if L == R:
                 invalid += i
 
@@ -21,35 +25,31 @@ print('Part 1:', invalid)
 
 invalid = 0
 
-def getSubStrings(s, l):
+def get_substrings(s, l):
     return [s[i:i+l] for i in range(0, len(s), l)]
 
 def try_match(str_i):
-    for l in range(1,len(str_i)+1):
-        matches = 0
-        mismatch = False
-        strings = getSubStrings(str_i, l)
+    # Try to match patterns of length 1,2,3...
+    for l in range(1,len(str_i)):
+        # Split strings into equal lengths
+        strings = get_substrings(str_i, l)
+        # Only 1 string means we've gone past halfway
         if len(strings) == 1:
-            continue
-        if len(str_i)%len(strings[0]) > 0 :
-            continue
-        #print('%%',strings)
-        for thing in strings[1:]:
-            if thing == strings[0]:
-                matches += 1
-            else:
-                mismatch = True
-
-        if not mismatch and matches >= 1:
-            print(strings, matches)
-            print('*', i)
+            return 0
+        # Check all the strings in the list are the same
+        matches = set(strings)
+        # If there's only one set element then that's a match
+        if len(matches) == 1:
             return i
     return 0
 
+# Iterate over the ranges
 for RANGE in ranges:
     l,r = [int(x) for x in RANGE.split('-')]
+    # Iterate over the l->r range
     for i in range(l,r+1):
         str_i = str(i)
+        # Try to match patterns in the current string
         invalid += try_match(str_i)
 
 print('Part 2:', invalid)
