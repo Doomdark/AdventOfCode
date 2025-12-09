2e78128fed85076307c2c892e4327cb5a204c8d9
Doomdark <54108031+Doomdark@users.noreply.github.com>
2025-12-09 22:44:53 +0000

    Update 09.py

--- 2025/09.py
+++ 2025/09.py
@@ -72,7 +72,8 @@ startx = min([x for x,y in edges])
 starty = min([y for x,y in tiles if x == startx])
 fill((startx+1, starty+1))
 
-print_grid()
+#print_grid()
+#exit(1)
 
 biggest = 0
 
@@ -82,19 +83,33 @@ for tile1 in tiles:
         x2,y2 = tile2
         if tile2 == tile1: continue
 
-        # Test if the area covered by these corners is all filled
+        # Test if the rectangle perimeter covered by these corners is in the filled set
         covered = True
-        for ny in range(min(y1,y2),max(y1,y2)+1):
-            for nx in range(min(x1,x2), max(x1,x2)+1):
+        min_x, max_x = min(x1,x2), max(x1,x2)
+        min_y, max_y = min(y1,y2), max(y1,y2)
+
+        for ny in [min_y,max_y]:
+            for nx in range(min_x, max_x+1):
                 if (nx,ny) not in filled:
-                   covered = False
-                   break
+                    covered = False
+                    break
+            if not covered:
+                break
+
+        if not covered:
+            continue
+
+        for nx in [min_x,max_x]:
+            for ny in range(min_y, max_y+1):
+                if (nx,ny) not in filled:
+                    covered = False
+                    break
             if not covered:
                 break
 
         if covered:
-            x = max(x1,x2) - min(x1,x2) + 1
-            y = max(y1,y2) - min(y1,y2) + 1
+            x = max_x - min_x + 1
+            y = max_y - min_y + 1
             area = x * y
             biggest = max(area, biggest)
 
