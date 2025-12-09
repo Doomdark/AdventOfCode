a97f159d8af0ec9c24b5781959f55e4b12752059
Doomdark <54108031+Doomdark@users.noreply.github.com>
2025-12-09 21:20:46 +0000

    Update 08.py

--- 2025/08.py
+++ 2025/08.py
@@ -1,10 +1,12 @@
 import numpy as np
+import networkx.utils.union_find as uf
 
 # Read the input
-lines = open("08a.in").read().splitlines()
+lines = open("08.in").read().splitlines()
 
-boxes = {}
-circuits = []
+boxes = []
+dists = set()
+conns = []
 
 def ed(a,b):
     'Euclidean distance'
@@ -15,40 +17,47 @@ def ed(a,b):
 
 for line in lines:
     x,y,z = [int(a) for a in line.split(',')]
-    boxes[(x,y,z)] = 1
+    boxes.append((x,y,z))
 
 # Fid the nearest other box
 for box1 in boxes:
-    nearest = None
     for box2 in boxes:
         if box1 == box2: continue
         distance = ed(box1,box2)
-        if nearest is None or distance < nearest[1]:
-            nearest = (box2, distance)
-    boxes[box1] = nearest
+        # Don't repeat the distances. It's v unlikely they'll be duplicated.
+        if distance not in dists:
+            conns.append((distance, box1, box2))
+            dists.add(distance)
 
-# Now make the circuits
-num = 0
-# use the shortest distances between boxes as the sort
-for box, nearest in sorted(boxes.items(), key=lambda x: x[1][1]):
-    print(box, nearest)
-    if num < 10:
-        # No circuit so make the first
-        if len(circuits) == 0:
-            circuits.append({box, nearest[0]})
-        else:
-            found = False
-            for circuit in circuits:
-                if not found and nearest[0] in circuit:
-                    circuit.add(box)
-                    found = True
-            if not found: # Make a new circuit
-                circuits.append({box, nearest[0]})
-    else:
-        circuits.append({box})
-    num += 1
+# Sort the connections by distance
+conns.sort()
 
-for a in circuits:
-    print('---')
-    for b in a:
-        print(b)
+# Make a list with the first 1000 connections in
+closest = []
+for conn in conns[:1000]:
+    closest.append((conn[1],conn[2]))
+
+# Merge the connections
+ds = uf.UnionFind()
+sets = []
+for g in closest: ds.union(*g)
+for s in ds.to_sets(): sets.append(len(s))
+sets.sort()
+
+# The biggest 3 sets need multiplying together
+print('Part 1:', sets[-1]*sets[-2]*sets[-3])
+
+# Make a list with all the connections in
+closest = []
+for conn in conns:
+    closest.append((conn[1],conn[2]))
+
+# Go through all of the connections and merge them until there is only 1 set
+for i in range(2,len(closest)):
+    sets = []
+    for g in closest[:i]: ds.union(*g)
+    for s in ds.to_sets(): sets.append(s)
+    # Check there's only 1 set and that the set length is the same as the number of boxes
+    if len(sets) == 1 and len(sets[0]) == len(lines):
+        print('Part 2:', closest[i-1][0][0] * closest[i-1][1][0])
+        exit(0)
