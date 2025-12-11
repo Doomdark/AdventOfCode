a082443c96087ab00d0b795254ce661d38a0bc5d
Doomdark <54108031+Doomdark@users.noreply.github.com>
2025-12-11 09:25:36 +0000

    Update 11.py

--- 2025/11.py
+++ 2025/11.py
@@ -1,5 +1,5 @@
 # Read the input
-lines = open("11b.in").read().splitlines()
+lines = open("11.in").read().splitlines()
 
 devices = {}
 
@@ -8,32 +8,39 @@ for line in lines:
     OUTS_ = OUTS.split()
     devices[IN] = OUTS_
 
-DP = {}
-trails = set()
+# out doesn't have a node
+devices['out'] = []
 
-def dfs(device, trail):
-    # Already seen this combination?
-    if device in DP:
-        return DP[device]
-    paths = 0
-    ntrail = trail + [device]
-    trails = 0
-    moves = devices[device]
-    for move in moves:
-        if move == 'out':
-            paths += 1
-            print(ntrail + ['out'])
-            if 'dac' in ntrail and 'fft' in ntrail:
-                trails += 1
-        else:
-            _paths, _trails = dfs(move, ntrail)
-            paths  += _paths
-            if _trails >= 1:
-                trails *= _trails
-    DP[device] = (paths, trails)
-    return (paths, trails)
+def num_paths(start, end):
+    # Cache the paths
+    DP = {}
 
-p, t = dfs('svr', [])
+    def dfs(device):
+        # Already seen this combination?
+        if device in DP: return DP[device]
+        # Initialise paths
+        paths = 0
+        # Go to all the output devices
+        for move in devices[device]:
+            # Are we at the end?
+            if move == end: paths += 1
+            # Nope, more iteration needed
+            else: paths += dfs(move)
+        # Say we've been here
+        DP[device] = paths
+        return paths
 
-print('Part 1:', p)
-print('Part 2:', t)
+    return dfs(start)
+
+print('Part 1:', num_paths('you', 'out'))
+
+# Multiply up all the paths from svr->fft->dac->out and svr-dac->fft->out as
+# doing all the paths is too much in one go.
+svr_to_dac = num_paths('svr', 'dac')
+dac_to_fft = num_paths('dac', 'fft')
+fft_to_out = num_paths('fft', 'out')
+svr_to_fft = num_paths('svr', 'fft')
+fft_to_dac = num_paths('fft', 'dac')
+dac_to_out = num_paths('dac', 'out')
+
+print('Part 2:', (svr_to_dac*dac_to_fft*fft_to_out) + (svr_to_fft*fft_to_dac*dac_to_out))
